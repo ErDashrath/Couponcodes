5f296a65d8388343efec53cb83d7ff042fa7c122
ErDashrath <dasharahtparekar2002@gmail.com>
2024-08-27 22:52:33 +0530

    SECOND COMMIT

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,28 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+
+// Uncomment and require these when ready
 const bodyParser = require('body-parser');
-const cors = require('cors');
-const AuthRouter = require('./Routes/AuthRouter');
+ const cors = require('cors');
+ const AuthRouter = require('./Routes/AuthRouter');
 const ProductRouter = require('./Routes/ProductRouter');
 
-require('dotenv').config();
+// Database connection (Uncomment when ready)
 require('./Models/db');
+
 const PORT = process.env.PORT || 8080;
 
 app.get('/ping', (req, res) => {
     res.send('PONG');
 });
 
+// Uncomment these when ready to use
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/auth', AuthRouter);
 app.use('/products', ProductRouter);
 
-
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
-})
-//dsp
+    console.log(`Server is running on ${PORT}`);
+});
